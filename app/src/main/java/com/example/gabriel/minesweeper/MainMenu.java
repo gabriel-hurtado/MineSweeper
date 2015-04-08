3fb17b6bd5fa96ad41f958d7de9a2c0d80e9de11
gabriel-hurtado <gabriel.j.hurtado@gmail.com>
2015-04-08 12:33:30 +0200

    Created main activities

--- app/src/main/java/com/example/gabriel/minesweeper/MainMenu.java
+++ app/src/main/java/com/example/gabriel/minesweeper/MainMenu.java
@@ -1,9 +1,11 @@
 package com.example.gabriel.minesweeper;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class MainMenu extends ActionBarActivity {
@@ -15,25 +17,14 @@ public class MainMenu extends ActionBarActivity {
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main_menu, menu);
-        return true;
+    public void showHelp (View clickedButton) {
+        Intent in = new Intent(this, HelpActivity.class);
+        startActivity(in);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    public void showGameStarter (View clickedButton) {
+        Intent in = new Intent(this, GameStarterActivity.class);
+        startActivity(in);
     }
 }
