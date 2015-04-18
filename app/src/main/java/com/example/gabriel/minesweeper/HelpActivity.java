959eb6023cc5e3fa32fe55e5ea996914bc3a6431
maxime <maxime.churin@orange.fr>
2015-04-18 16:32:26 +0200

    Non impementation des methodes onClick des buttons

--- app/src/main/java/com/example/gabriel/minesweeper/HelpActivity.java
+++ app/src/main/java/com/example/gabriel/minesweeper/HelpActivity.java
@@ -1,9 +1,11 @@
 package com.example.gabriel.minesweeper;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class HelpActivity extends ActionBarActivity {
@@ -12,6 +14,11 @@ public class HelpActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
+    }
+
+    public void showMainMenu (View clickedButton) {
+        Intent in = new Intent(this, MainMenu.class);
+        startActivity(in);
     }
 
 
