2b7b461645f662e3614c50c51ea6c867aee102ad
gabriel-hurtado <gabriel.j.hurtado@gmail.com>
2015-04-18 19:44:58 +0200

    Creation of the grid

--- app/src/main/java/com/example/gabriel/minesweeper/GameActivity.java
+++ app/src/main/java/com/example/gabriel/minesweeper/GameActivity.java
@@ -3,6 +3,7 @@ package com.example.gabriel.minesweeper;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -17,11 +18,13 @@ public class GameActivity extends Activity {
     private int size;
     private String UserName;
     private grid gameGeneration;
+    private TextView t;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        t=(TextView)findViewById(R.id.myTextView);
         in = getIntent();
         infos = in.getExtras();
         timer = infos.getBoolean("timer");
@@ -30,7 +33,8 @@ public class GameActivity extends Activity {
         UserName = infos.getString("UserName");
         gameGeneration= new grid(size);
         gameGeneration.putMines(generateMines(percentage,size));
-
+        gameGeneration.addNumberOfMinesNear();
+        gameGeneration.test();
 
     }
 
@@ -39,18 +43,53 @@ public class GameActivity extends Activity {
 
         public grid(int size){
            this.gridArray= new int[size][size];
-         /*inizialize to 0*/
         }
 
         private void putMines(int[] minesArray){
-          /*put the mines at their place*/
+          for(int mineSize: minesArray){
+           this.gridArray[mineSize/size][mineSize%size]=-1;
+          }
         }
 
+        private void addNumberOfMinesNear(){
+         for(int i=0;i<size;i++){
+            for(int j=0;j<size;j++) {
+                if (this.gridArray[i][j]!=-1){
+                    gridArray[i][j]=this.minesNear(i, j);
+                }
+            }
+         }
+        }
+
+        private int minesNear(int i,int j){
+             int value=0;
+            /*counter the number of mines near and return an int*/
+            for(int bouclei=0;i<size;i++){
+                for(int bouclej=0;j<size;j++) {
+                    if (this.gridArray[bouclei][bouclej]==-1){
+                        if (((bouclei>=i-1)&&(bouclei<=i+1) && ((bouclej>=j-1)&&(bouclej<=j+1)))){
+                            value+=1;
+                        }
+                    }
+                }
+            }
+
+            return value;
+        }
+
+        private void test(){
+            for(int i = 0; i < size-1; i++) {
+                for (int j = 0; j < size - 1; j++) {
+                    t.append(""+this.gridArray[i][j]);
+                }
+                t.append("\n");
+            }
+        }
 
     }
 
     public int[] generateMines(double percentage, int size){
-        List<Integer> list = new ArrayList<Integer>();
+        List<Integer> list = new ArrayList<>();
         int[] listArray;
         int numberOfMines = (int) ((size*size)*percentage);
         for(int i = 0; i < numberOfMines-1;)
@@ -74,7 +113,7 @@ public class GameActivity extends Activity {
         Iterator<Integer> iterator = integers.iterator();
         for (int i = 0; i < ret.length; i++)
         {
-            ret[i] = iterator.next().intValue();
+            ret[i] = iterator.next();
         }
         return ret;
     }
