0d359c81ccf1929aa32d88668a3f3d58ac169748
gabriel-hurtado <gabriel.j.hurtado@gmail.com>
2015-04-19 16:45:47 +0200

    Tried to implement the GridView

--- app/src/main/java/com/example/gabriel/minesweeper/GameActivity.java
+++ app/src/main/java/com/example/gabriel/minesweeper/GameActivity.java
@@ -1,13 +1,21 @@
 package com.example.gabriel.minesweeper;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-//import android.widget.TextView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.GridView;
 
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+
+//import android.widget.TextView;
 
 
 public class GameActivity extends Activity {
@@ -17,13 +25,14 @@ public class GameActivity extends Activity {
     private double percentage;
     private int size;
     private String UserName;
-    private grid gameGeneration;
+    private GridView gridView;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        gridView = (GridView) findViewById(R.id.gridview);
         // test TextView t=(TextView)findViewById(R.id.myTextView);
         in = getIntent();
         gameLog = in.getExtras();
@@ -31,11 +40,23 @@ public class GameActivity extends Activity {
         percentage = gameLog.getDouble("percentage");
         size = Integer.parseInt(gameLog.getString("size"));
         UserName = gameLog.getString("UserName");
-        gameGeneration= new grid(size, percentage);
+
+        gridView.setNumColumns(size);
+        gridView.setAdapter(new ButtonAdapter(this));
 
         if(timer){
             //showTimer();
         }
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id)
+            {
+                //reactionar al click
+            }
+        });
+
 
         //showGame(gameGeneration);
 
@@ -75,6 +96,8 @@ public class GameActivity extends Activity {
 
 
 
+
+
     public class grid {
       private int[][] gridArray;
 
@@ -84,6 +107,21 @@ public class GameActivity extends Activity {
             this.addNumberOfMinesNear();
         }
 
+        public int getSize(){
+            return this.gridArray[0].length;
+        }
+
+        public int[] toSimpleArray(){
+            int size=this.getSize();
+            int[] simpleArray= new int[size*size];
+            int counter=0;
+            for (int i=0;i<size;i++){
+                for (int j=0;j<size;j++){
+                  simpleArray[counter]=this.gridArray[i][j];
+                }
+            }
+            return simpleArray;
+        }
 
         private void putMines(int[] minesArray){
           for(int minePosition: minesArray){
@@ -166,4 +204,79 @@ public class GameActivity extends Activity {
    }
 
 
-}
+
+
+
+    //class of the adaptor
+    public class ButtonAdapter extends BaseAdapter {
+        private Context mContext;
+        private grid gameGeneration= new grid(size, percentage);
+
+
+        public ButtonAdapter(Context c) {
+            mContext = c;
+        }
+
+        public int getCount() {
+            return gameGeneration.getSize()*gameGeneration.getSize();
+        }
+
+        public Object getItem(int position) {
+            return null;
+        }
+
+        public long getItemId(int position) {
+            return position;
+        }
+
+        // create a new ImageView for each item referenced by the Adapter
+        public View getView(int position, View convertView, ViewGroup parent) {
+            Button btn;
+            if (convertView == null) {
+                // if it's not recycled, initialize some attributes
+                btn = new Button(mContext);
+                btn.setPadding(2, 2, 2, 2);
+            }
+            else {
+                btn = (Button) convertView;
+            }
+
+            switch (gameGeneration.toSimpleArray()[position]){
+                case 0:
+                 btn.setBackgroundResource(R.drawable.empty);
+                case 1:
+                 btn.setBackgroundResource(R.drawable.n1);
+                case 2:
+                    btn.setBackgroundResource(R.drawable.n2);
+                case 3:
+                    btn.setBackgroundResource(R.drawable.n3);
+                case 4:
+                    btn.setBackgroundResource(R.drawable.n4);
+                case 5:
+                    btn.setBackgroundResource(R.drawable.n5);
+                case 6:
+                    btn.setBackgroundResource(R.drawable.n6);
+                case 7:
+                    btn.setBackgroundResource(R.drawable.n7);
+                case 8:
+                    btn.setBackgroundResource(R.drawable.n8);
+                case -1:
+                    btn.setBackgroundResource(R.drawable.bomb);}
+            btn.setId(position);
+            return btn;
+        }
+    }
+        /* references to our images
+        private Integer[] drawable = {
+                R.drawable.undiscovered,
+                R.drawable.n1,
+                R.drawable.n2,
+                R.drawable.n3,
+                R.drawable.n4,
+                R.drawable.n5,
+                R.drawable.n6,
+                R.drawable.n7,
+                R.drawable.n8,
+                R.drawable.bomb,
+        };*/
+    }
